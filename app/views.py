21dcedd664a2d66a1303d925041a04b04cdce45a
runningstrawberry <elfishing07@gmail.com>
2016-06-08 03:07:20 +0100

    seperate the html and python

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 from werkzeug.utils import secure_filename
 from gridfs.errors import NoFile
 from bson.objectid import ObjectId
+from HTMLParser import HTMLParser
 
 ALLOWED_EXTENSIONS = set(['apk'])
 fs = gridfs.GridFS(MongoClient().pancake)
@@ -31,48 +32,17 @@ def upload_file():
         if file and allowed_file(file.filename):
         	filename = secure_filename(file.filename)
         	oid = fs.put(file, content_type=file.content_type, filename=filename)
-    		return redirect(url_for('upload_file', oid=str(oid)))
-
-    return '''
-    <!doctype html>
-    <html>
-    <head>
-    <title>Upload new File</title>
-    </head>
-    <body>
-    <h1>Upload new File</h1>
-    <form action="" method="post" enctype="multipart/form-data">
-      <p><input type="file" name="file">
-         <input type="submit" value="Upload">
-    </form>
-    <a href="%s">All files</a>
-    </body>
-    </html>
-    ''' % url_for('list_gridfs_files')
-
+    		redirect(url_for('upload_file', oid=str(oid)))
+    
+    return render_template('upload_file.html')
 
 
 @app.route('/files')
 def list_gridfs_files():
     files = [fs.get_last_version(file) for file in fs.list()]
-    file_list = "\n".join(['<li><a href="%s">%s</a></li>' % \
-                            (url_for('serve_gridfs_file', oid=str(file._id)), file.name) \
-                            for file in files])
-    return '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>Files</title>
-    </head>
-    <body>
-    <h1>Files</h1>
-    <ul>
-    %s
-    </ul>
-    <a href="%s">Upload new file</a>
-    </body>
-    </html>
-    ''' % (file_list, url_for('upload_file'))
+    oid=[str(file._id) for file in files] 
+    return render_template('list_gridfs_files.html',oid =oid, files=files)   
+
 
 @app.route('/files/<oid>')
 def serve_gridfs_file(oid):
